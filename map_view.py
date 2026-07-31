07d4eb864841f269418eff8a051167a0c57ac09f
n-boop553 <nakatani.mh@gmail.com>
2026-07-31 23:36:27 +0900

    Create map_view.py

---
+++ map_view.py
@@ -0,0 +1,17 @@
+import plotly.graph_objects as go
+
+
+def create_route_figure(
+    route,
+    start_id,
+    goal_id,
+    places,
+    roads,
+    place_by_id,
+    food_places,
+):
+    figure = go.Figure()
+
+    # 現在のcreate_route_figure関数の内容をここへ移す
+
+    return figure
